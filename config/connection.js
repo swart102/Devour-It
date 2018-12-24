42f7cf7144f35bca65c3f7c6ce31d4f18ac0898b
swart.isa <swart102@umn.edu>
2018-12-23 23:04:30 -0600

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    conection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "hacktheplanet",
+      database: "todoagain_db"
+    });
+};
 
 connection.connect(function(err) {
   if (err) {
